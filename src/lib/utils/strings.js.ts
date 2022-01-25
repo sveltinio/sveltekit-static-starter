7863f771f0549090301cb8147841e7e6ffb50151
indaco <github@mircoveltri.me>
2022-01-25 10:10:35 +0100

    getCoverImagePath method added to lib/strings

--- src/lib/utils/strings.js.ts
+++ src/lib/utils/strings.js.ts
@@ -1,3 +1,6 @@
+import type { IWebSite } from '@sveltinio/seo/types';
+import type { ResourceContent } from '@sveltinio/widgets/types';
+
 export const textToSlug = (text: string): string => {
 	return text
 		.toLowerCase()
@@ -11,4 +14,19 @@ export const convertToSentenceCase = (text: string): string => {
 
 export const isNotEmpty = (text: string): boolean => {
 	return Boolean(text);
+};
+
+export const getCoverImagePath = (
+	urlData: URL,
+	item: ResourceContent,
+	websiteData: IWebSite
+): string => {
+	if (item.metadata.cover && isNotEmpty(item.metadata.cover)) {
+		return (
+			urlData.origin +
+			'/' +
+			`resources/${item.resource}/${item.metadata.slug}/${item.metadata.cover}`
+		);
+	}
+	return urlData.origin + '/' + websiteData.favicon;
 };
