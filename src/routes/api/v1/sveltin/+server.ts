5e7787b72befe4e372dccf7c4cc7608e7b89153a
Mirco Veltri <github@mircoveltri.me>
2022-09-05 17:09:00 +0200

    fix: add prerender to the api file

--- src/routes/api/v1/sveltin/+server.ts
+++ src/routes/api/v1/sveltin/+server.ts
@@ -1,5 +1,7 @@
 import { sveltinVersion, sveltekitVersion, buildTime } from '$config/defaults.js';
 
+export const prerender = true;
+
 /** @type {import('./$types').RequestHandler} */
 export function GET(): Response {
 	return new Response(
