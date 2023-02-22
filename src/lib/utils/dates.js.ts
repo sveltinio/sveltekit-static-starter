8a8335dd7ebe6cb6753377f3af5df67eedd137cb
Mirco Veltri <github@mircoveltri.me>
2023-02-22 14:27:40 +0100

    chore(lib/utils): updated to adhere to sveltin v0.11.0

--- src/lib/utils/dates.js.ts
+++ src/lib/utils/dates.js.ts
@@ -1,18 +1,14 @@
-export function padTo2Digits(num: number) {
-	return num.toString().padStart(2, '0');
-}
-export function formatDate(date: Date) {
-	return [padTo2Digits(date.getDate()), padTo2Digits(date.getMonth() + 1), date.getFullYear()].join(
-		'/'
-	);
-}
+export const padTo2Digits = (num: number): string => num.toString().padStart(2, '0');
 
-export function dayInMonthFromDate(dateStr: string): number {
+export const formatDate = (date: Date): string =>
+	[padTo2Digits(date.getDate()), padTo2Digits(date.getMonth() + 1), date.getFullYear()].join('/');
+
+export const dayInMonthFromDate = (dateStr: string): number => {
 	const dt = new Date(dateStr);
 	return dt.getDate();
-}
+};
 
-export function monthShortFromDate(dateStr: string): string {
+export const monthShortFromDate = (dateStr: string): string => {
 	const dt = new Date(dateStr);
 	return dt.toLocaleString('default', { month: 'short' });
-}
+};
--- src/lib/utils/strings.js.ts
+++ src/lib/utils/strings.js.ts
@@ -1,52 +1,49 @@
-import type { IWebSite } from '@sveltinio/seo/types';
-import type { Sveltin } from 'src/sveltin';
+import type { Sveltin } from '$sveltin';
 
-export const ToTitle = (text: string): string => {
-	return CapitalizeAll(text.replace(/-/g, ' '));
+export const capitalizeFirstLetter = (text: string): string => {
+	return text.charAt(0).toUpperCase() + text.substring(1).toLowerCase();
 };
 
-export const CapitalizeAll = (text: string): string => {
+export const capitalizeAll = (text: string): string => {
 	const splitted = text.toLowerCase().split(' ');
 	const capitalized: Array<string> = [];
 
 	splitted.forEach(function (item) {
-		capitalized.push(CapitalizeFirstLetter(item));
+		capitalized.push(capitalizeFirstLetter(item));
 	});
 	return capitalized.join(' ');
 };
 
-export const CapitalizeFirstLetter = (text: string): string => {
-	return text.charAt(0).toUpperCase() + text.substring(1).toLowerCase();
-};
+export const toTitle = (text: string): string => capitalizeAll(text.replace(/-/g, ' '));
 
-export const ToSlug = (text: string): string => {
-	return text
+export const toSlug = (text: string): string =>
+	text
 		.toLowerCase()
 		.replace(/[^\w ]+/g, '')
 		.replace(/ +/g, '-');
-};
 
-export const CamelToKebabCase = (text: string): string => {
-	return text.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
-};
+export const camelToKebabCase = (text: string): string =>
+	text.replace(/([a-z0-9])([A-Z])/g, '$1-$2').toLowerCase();
 
-export const isNotEmpty = (text: string): boolean => {
-	return Boolean(text);
-};
+export const isNotEmpty = (text: string): boolean => Boolean(text);
 
-export const getPageUrl = (name: string, websiteData: IWebSite): string => {
-	return websiteData.baseURL.concat('/', name);
-};
+export const getPageUrl = (name: string, websiteData: Sveltin.WebSite): string =>
+	websiteData.baseURL.concat('/', name);
 
-export const getSlugPageUrl = (item: Sveltin.ContentEntry, websiteData: IWebSite): string => {
+export const getSlugPageUrl = (
+	item: Sveltin.ResourceContent,
+	websiteData: Sveltin.WebSite
+): string => {
 	return websiteData.baseURL.concat('/', item.resource, '/', item.metadata.slug);
 };
 
-export const getFavicon = (websiteData: IWebSite): string => {
-	return websiteData.baseURL.concat('/', websiteData.favicon);
-};
+export const getFavicon = (websiteData: Sveltin.WebSite): string =>
+	websiteData.baseURL.concat('/', websiteData.favicon);
 
-export const getCoverImagePath = (item: Sveltin.ContentEntry, websiteData: IWebSite): string => {
+export const getCoverImagePath = (
+	item: Sveltin.ResourceContent,
+	websiteData: Sveltin.WebSite
+): string => {
 	if (item.metadata.cover && isNotEmpty(item.metadata.cover)) {
 		return websiteData.baseURL.concat(
 			'/',
@@ -55,3 +52,5 @@ export const getCoverImagePath = (item: Sveltin.ContentEntry, websiteData: IWebS
 	}
 	return getFavicon(websiteData);
 };
+
+export const canonicalPageUrl = (name: string, baseURL: string): string => baseURL.concat(name);
